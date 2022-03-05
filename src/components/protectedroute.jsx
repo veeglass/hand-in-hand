4a2fb018ef7ff9a6bdb79c39529f427e23f30337
Ijarotimi Tomilayo <moladeijarotimi@gmail.com>
2022-03-05 11:57:23 +0100

    initial commit

--- src/components/protectedroute.jsx
+++ src/components/protectedroute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useMoralis } from "react-moralis";
-import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom"
 
 const ProtectedRoute = () => {
     const { user } = useMoralis();
